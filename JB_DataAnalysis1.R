cb84e321cac9a2406de93bdf756cd2d09fca263b
Jonty-JNQ <jonathan.sinai@rmb.co.za>
2017-02-21 07:38:50 +0200

    Latest Hello World

---
+++ JB_DataAnalysis1.R
@@ -0,0 +1,17 @@
+## Working directory checked. Workspace sound. We're connected to GitHub. 
+
+#We'll be using open source data from the Gapminder project (http://www.gapminder.org/)
+
+install.packages("gapminder")
+
+# Then load the package (after running the install)
+
+library(gapminder)
+
+# Now Load the Package
+
+library(gapminder)
+
+# Time to access the data frame. Hint: str stands fro structure
+
+str(gapminder)
---
+++ aRrgh.r
@@ -0,0 +1,9 @@
+## Some R Reinforcement (http://arrgh.tim-smith.us/)
+
+## Part 1:  Basic Syntax and Gotchas
+
+# ; or newline separates commands
+
+# Variables have functional scope
+
+# White space is meaningless, but sometimes it is.
